166afdad86077e0e2dd120749e341e340b42c202
Zac H <zac.haluza@gmail.com>
2020-08-07 23:44:25 -0400

    update linting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
-    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint', 'plugin:prettier/recommended', 'plugin:react/recommended'],
     parserOptions: {
         ecmaVersion: 2020,
         sourceType: 'module',
@@ -8,7 +8,14 @@ module.exports = {
             jsx: true,
         },
     },
-    rules: {},
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+    rules: {
+        'react/jsx-uses-react': 1
+    },
     settings: {
         react: {
             version: 'detect',
