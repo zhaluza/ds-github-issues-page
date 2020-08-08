24175e0ff4b563409a29087eb173e9300470d167
Zac H <zac.haluza@gmail.com>
2020-08-07 23:45:10 -0400

    set up basic app component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './sass/app.scss';
+import RepoHeader from './components/RepoHeader';
+import RepoFilterBar from './components/RepoFilterBar';
+import IssuesContainer from './components/IssuesContainer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App: React.FC = () => (
+    <div className="app">
+        <RepoHeader />
+        <RepoFilterBar />
+        <IssuesContainer />
     </div>
-  );
-}
+);
 
 export default App;
