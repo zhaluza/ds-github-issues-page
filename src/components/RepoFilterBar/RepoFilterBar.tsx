7d4dc25ec24f8ba4611906b537ca195cc6360b93
Zac H <zac.haluza@gmail.com>
2020-08-09 16:37:35 -0400

    add filter content

--- src/components/RepoFilterBar/RepoFilterBar.tsx
+++ src/components/RepoFilterBar/RepoFilterBar.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const RepoFilterBar: React.FC = () => {
-    return <div></div>;
+    return (
+        <div>
+            <div className="bar-1">
+                <button>Filters</button>
+                <input type="text" placeholder="is:issue is:open" />
+            </div>
+            <div className="bar-2">
+                <button>
+                    <i className="fas fa-tags"></i>Labels
+                </button>
+                <button>
+                    <i className="fas fa-map-signs"></i>Milestones
+                </button>
+            </div>
+            <button>New Issue</button>
+        </div>
+    );
 };
 
 export default RepoFilterBar;
