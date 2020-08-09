3d190a6464120086da0abfc49f308ed7b3b6d477
Zac H <zac.haluza@gmail.com>
2020-08-09 19:13:30 -0400

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './sass/app.scss';
+import Navbar from './components/Navbar';
 import RepoHeader from './components/RepoHeader';
 import RepoFilterBar from './components/RepoFilterBar';
 import IssuesContainer from './components/IssuesContainer';
 
 const App: React.FC = () => (
     <div className="app">
+        <Navbar />
         <RepoHeader />
         <RepoFilterBar />
         <IssuesContainer />
