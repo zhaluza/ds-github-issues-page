b93d7c20a5b75fae98f839c97e73461c238a074c
Zac H <zac.haluza@gmail.com>
2020-08-10 00:19:06 -0400

    add mobile markup

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import './navbar.scss';
 
-const Navbar: React.FC = () => {
-    return (
+interface NavbarProps {
+    isMobile: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isMobile }: NavbarProps) => {
+    const [menuOpen, setMenuOpen] = useState<boolean>(false);
+    return isMobile ? (
+        <Fragment>
+            <div className="navbar navbar-mobile">
+                <i className="fas fa-bars" onClick={() => setMenuOpen((prev) => !prev)}></i>
+                <i className="fab fa-github"></i>
+                <i className="fas fa-bell"></i>
+            </div>
+            {menuOpen && (
+                <div className="menu-stack">
+                    <ul>
+                        <li>
+                            <input type="text" placeholder="Search GitHub" />
+                        </li>
+                        <li>
+                            <a href="https://github.com/dashboard">Dashboard</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/pulls">Pull requests</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/issues">Issues</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/marketplace">Marketplace</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/explore">Explore</a>
+                        </li>
+                        <li>
+                            <a href="https://github.com">Sign Out</a>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </Fragment>
+    ) : (
         <div className="navbar">
             <div className="navbar__left">
                 <i className="fab fa-github"></i>
@@ -18,6 +58,7 @@ const Navbar: React.FC = () => {
                 <details>
                     <summary>
                         <i className="fas fa-plus"></i>
+                        <i className="fas fa-caret-down"></i>
                     </summary>
                 </details>
             </div>
