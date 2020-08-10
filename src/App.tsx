0390c2f395fe90100238a29e048de29f5a1abb09
Zac H <zac.haluza@gmail.com>
2020-08-10 00:08:17 -0400

    add detection for smaller screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './sass/app.scss';
 import Navbar from './components/Navbar';
 import RepoHeader from './components/RepoHeader';
 import RepoFilterBar from './components/RepoFilterBar';
 import IssuesContainer from './components/IssuesContainer';
 
-const App: React.FC = () => (
-    <div className="app">
-        <Navbar />
-        <RepoHeader />
-        <RepoFilterBar />
-        <IssuesContainer />
-    </div>
-);
+const App: React.FunctionComponent = () => {
+    const [isMobile, setIsMobile] = useState<boolean>(true);
+    // detect window size on render
+    useEffect(() => {
+        if (window.innerWidth > 700) {
+            setIsMobile(false);
+        }
+    }, []);
+    return (
+        <div className="app">
+            <Navbar isMobile={isMobile} />
+            <RepoHeader isMobile={isMobile} />
+            <RepoFilterBar isMobile={isMobile} />
+            <IssuesContainer isMobile={isMobile} />
+        </div>
+    );
+};
 
 export default App;
