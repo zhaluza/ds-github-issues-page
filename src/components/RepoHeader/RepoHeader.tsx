5fc41e38a91291e5f4899ee742436116f5eefebd
Zac H <zac.haluza@gmail.com>
2020-08-10 00:20:25 -0400

    add mobile markup

--- src/components/RepoHeader/RepoHeader.tsx
+++ src/components/RepoHeader/RepoHeader.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './repoHeader.scss';
 
-const RepoHeader: React.FC = () => {
+interface RepoHeaderProps {
+    isMobile: boolean;
+}
+
+const RepoHeader: React.FC<RepoHeaderProps> = ({ isMobile }: RepoHeaderProps) => {
     return (
         <div className="repo-header">
             <div className="header__top">
@@ -11,26 +15,25 @@ const RepoHeader: React.FC = () => {
                         react
                     </a>
                 </h2>
-                <div className="header__top-right">
-                    <div className="header__top-watch">
-                        <button>
-                            <i className="far fa-eye"></i>Watch
-                        </button>
-                        <button>6.7k</button>
+                {!isMobile && (
+                    <div className="header__top-right">
+                        <div className="header__top-watch">
+                            <button>
+                                <i className="far fa-eye"></i>Watch
+                            </button>
+                        </div>
+                        <div className="header__top-star">
+                            <button>
+                                <i className="far fa-star"></i>Star
+                            </button>
+                        </div>
+                        <div className="header__top-fork">
+                            <button>
+                                <i className="fas fa-code-branch"></i>Fork
+                            </button>
+                        </div>
                     </div>
-                    <div className="header__top-star">
-                        <button>
-                            <i className="far fa-star"></i>Star
-                        </button>
-                        <button>154k</button>
-                    </div>
-                    <div className="header__top-fork">
-                        <button>
-                            <i className="fas fa-code-branch"></i>Fork
-                        </button>
-                        <button>30.1k</button>
-                    </div>
-                </div>
+                )}
             </div>
             <div className="header__bottom">
                 <div className="header__bottom-options">
@@ -43,21 +46,25 @@ const RepoHeader: React.FC = () => {
                     <a href="https://github.com/facebook/react/pulls">
                         <i className="fas fa-project-diagram"></i>Pull requests
                     </a>
-                    <a href="https://github.com/facebook/react/actions">
-                        <i className="far fa-play-circle"></i>Actions
-                    </a>
-                    <a href="https://github.com/facebook/react/projects">
-                        <i className="fas fa-tasks"></i>Projects
-                    </a>
-                    <a href="https://github.com/facebook/react/wiki">
-                        <i className="fas fa-book-open"></i>Wiki
-                    </a>
-                    <a href="https://github.com/facebook/react/security">
-                        <i className="fas fa-shield-alt"></i>Security
-                    </a>
-                    <a href="https://github.com/facebook/react/pulse">
-                        <i className="fas fa-chart-line"></i>Insights
-                    </a>
+                    {!isMobile && (
+                        <Fragment>
+                            <a href="https://github.com/facebook/react/actions">
+                                <i className="far fa-play-circle"></i>Actions
+                            </a>
+                            <a href="https://github.com/facebook/react/projects">
+                                <i className="fas fa-tasks"></i>Projects
+                            </a>
+                            <a href="https://github.com/facebook/react/wiki">
+                                <i className="fas fa-book-open"></i>Wiki
+                            </a>
+                            <a href="https://github.com/facebook/react/security">
+                                <i className="fas fa-shield-alt"></i>Security
+                            </a>
+                            <a href="https://github.com/facebook/react/pulse">
+                                <i className="fas fa-chart-line"></i>Insights
+                            </a>
+                        </Fragment>
+                    )}
                 </div>
             </div>
         </div>
