d74d766c9630c585392cf8138d0f5692a5b9f887
Zac H <zac.haluza@gmail.com>
2020-08-10 00:19:31 -0400

    add mobile markup

--- src/components/RepoFilterBar/RepoFilterBar.tsx
+++ src/components/RepoFilterBar/RepoFilterBar.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 import './repoFilterBar.scss';
 
-const RepoFilterBar: React.FC = () => {
-    return (
-        <div className="repo-filter-bar">
-            <div className="bar bar-1">
+interface RepoFilterProps {
+    isMobile: boolean;
+}
+
+const RepoFilterBar: React.FC<RepoFilterProps> = ({ isMobile }: RepoFilterProps) => {
+    return isMobile ? (
+        <div className="repo-filter-bar repo-filter-mobile">
+            <div className="filter__top">
+                <div className="filter__btns">
+                    <button>
+                        <i className="fas fa-tags"></i> Labels
+                    </button>
+                    <button>
+                        <i className="fas fa-map-signs"></i> Milestones
+                    </button>
+                </div>
+                <button className="btn-new-issue">New</button>
+            </div>
+            <div className="btn-input filter__bottom">
                 <button>Filters</button>
                 <input type="text" placeholder="is:issue is:open" />
             </div>
-            <div className="bar bar-2">
+        </div>
+    ) : (
+        <div className="repo-filter-bar">
+            <div className="btn-input bar-1">
+                <button>Filters</button>
+                <input type="text" placeholder="is:issue is:open" />
+            </div>
+            <div className="bar-2">
                 <button>
-                    <i className="fas fa-tags"></i>Labels
+                    <i className="fas fa-tags"></i> Labels
                 </button>
                 <button>
-                    <i className="fas fa-map-signs"></i>Milestones
+                    <i className="fas fa-map-signs"></i> Milestones
                 </button>
             </div>
-            <button>New Issue</button>
+            <button className="btn-new-issue">New Issue</button>
         </div>
     );
 };
